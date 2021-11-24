46d43df91cb783df98caf31d39e0c8fca1ebaa7e
yuyouyu <624297803@qq.com>
2021-11-24 16:42:26 +0800

    add(Project_Deployment): add arima module

--- Project_Deployment/arima/workdir/arima.py
+++ Project_Deployment/arima/workdir/arima.py
@@ -1,8 +1,12 @@
+import sys
+
+import joblib
 import numpy as np
+import pandas as pd
 import pmdarima as pm
 from matplotlib import pyplot as plt
-import pandas as pd
-import joblib
+
+from utils import _add_error_xml, _add_info_xml, _read_parameters
 
 
 def read_data(path, feature=None, target=[0]):
@@ -52,17 +56,24 @@ def arima_prdict_insample(arima, X=None):
 
 def arima(path, feature=None, target=[0], model_path="arima.pkl", file_path=None, n_periods=3, seasonal=False, m=0, information_criterion='bic', start_p=2, d=None,
           start_q=2, max_p=5, max_d=2, max_q=5):
-    feature_data, target_data, feature_columns, target_columns = read_data(path, feature=feature, target=target)
+    feature_data, target_data, feature_columns, target_columns = read_data(
+        path, feature=feature, target=target)
     arimas = arima_fit(feature_data, target_data,
                        seasonal=seasonal, m=m, information_criterion=information_criterion,
                        start_p=start_p, d=d, start_q=start_q, max_p=max_p, max_d=max_d, max_q=max_q)
     draw_bic_or_aic(arimas, information_criterion='bic', seasonal=False)
     if feature_data is None:
-        y_predict = arima_predict(model_path, file_path=None, n_periods=n_periods)
-        draw_predict(arimas[0], target_data,y_predict,feature=None)
+        y_predict = arima_predict(
+            model_path, file_path=None, n_periods=n_periods)
+        draw_predict(arimas[0], target_data, y_predict, feature=None)
     else:
-        y_predict = arima_predict(model_path, file_path=file_path, n_periods=n_periods)
-        draw_predict(arimas[0], target_data,y_predict, feature=feature_data)
+        y_predict = arima_predict(
+            model_path, file_path=file_path, n_periods=n_periods)
+        draw_predict(arimas[0], target_data, y_predict, feature=feature_data)
+    result_data = pd.read_csv(file_path)
+    result_data['prediction'] = y_predict
+    result_data.to_csv('result.csv', index=False)
+    return result_data
 
 
 def draw_bic_or_aic(arimas, information_criterion='bic', seasonal=False):
@@ -71,9 +82,9 @@ def draw_bic_or_aic(arimas, information_criterion='bic', seasonal=False):
     plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.rcParams['axes.unicode_minus'] = False
     if information_criterion == 'bic':
-        plt.title('网格遍历搜寻的各模型的bic结果')
+        plt.title('bic results for each model searched by grid traversal')
     elif information_criterion == 'aic':
-        plt.title('网格遍历搜寻的各模型的aic结果')
+        plt.title('aic results for each model searched by grid traversal')
     else:
         raise ValueError('information_criterion只能传递aic和bic两个值')
     for arima in arimas:
@@ -99,7 +110,7 @@ def draw_bic_or_aic(arimas, information_criterion='bic', seasonal=False):
     plt.clf()
 
 
-def draw_predict(arima, y, y_predict,feature=None):
+def draw_predict(arima, y, y_predict, feature=None):
     cur_predict = arima_prdict_insample(arima, feature)
     x = np.arange(len(cur_predict))
     plt.scatter(x, y, marker='x')
@@ -115,6 +126,43 @@ def draw_predict(arima, y, y_predict,feature=None):
     plt.clf()
 
 
+def main():
+    try:
+        parameters = _read_parameters()
+    except Exception as e:
+        _add_error_xml("Parameters Error", str(e))
+        with open('log.txt', 'a') as fp:
+            fp.write('error\n')
+        return
+    try:
+        result = arima(parameters['trainCSV'], feature=parameters['features'], target=parameters['target'],
+                   file_path=parameters['testCSV'], seasonal=parameters['seasonal'],
+                   m=parameters['m'], information_criterion=parameters['information_criterion'],
+                   start_p=parameters['start_p'], d=parameters['d'],
+                   start_q=parameters['start_q'], max_p=parameters['max_p'],
+                   max_d=parameters['max_d'], max_q=parameters['max_q'])
+    except Exception as e:
+        _add_error_xml("arima Error", str(e))
+        with open('log.txt', 'a') as fp:
+            fp.write('error\n')
+        return 
+    try:
+        _add_info_xml(picture_names=['result.jpg','aic_or_bic.jpg'], result=result)
+    except Exception as e:
+        _add_error_xml("XML Error", str(e))
+        with open('log.txt', 'a') as fp:
+            fp.write('error\n')
+        return
+  
+    with open('log.txt', 'a') as fp:
+        fp.write('finish\n')
+
+
+
 if __name__ == '__main__':
-    arima('test.csv', feature=None, target=[3],file_path="test2.csv", seasonal=False, m=5, information_criterion='bic', start_p=2, d=None,
-          start_q=2, max_p=5, max_d=2, max_q=5)
+    console = sys.stdout
+    file = open("task_log.txt", "w")
+    sys.stdout = file
+    main()
+    sys.stdout = console
+    file.close
--- Project_Deployment/arima/workdir/arima_API.py
+++ Project_Deployment/arima/workdir/arima_API.py
@@ -4,13 +4,16 @@ import tarfile
 
 # Get task id
 TID = sys.argv[1]
-module_name = 'LSTM'
+module_name = 'arima'
 
 # copy shap_func.py
 path_now = os.getcwd()
-command_cp_shap = f'copy D:\\{module_name}\\workdir\\{module_name}.py '+ path_now  # Windows
+command_cp_1 = f'copy D:\\{module_name}\\workdir\\{module_name}.py '+ path_now  # Windows
+command_cp_2 = f'copy D:\\{module_name}\\workdir\\utils.py '+ path_now  # Windows
 # command_cp_shap = f'cp ../{module_name}.py '+ path_now  # Linux
-os.system(command_cp_shap)
+# command_cp_shap = f'cp ../utils.py '+ path_now  # Linux
+os.system(command_cp_1)
+os.system(command_cp_2)
 
 command = f'python {module_name}.py'
 with open('log.txt', 'a') as fp:
@@ -20,9 +23,11 @@ os.system(command)
 
 os.system(f'del {module_name}.py')  # Windows
 # os.system(f'rm {module_name}.py')  # Linux
+os.system('del utils.py')  # Windows
+# os.system('rm utils.py')  # Linux
 
 with tarfile.open('./result.tar.gz', 'w:gz') as tar:
     for filename in os.listdir():
-        if filename in ['result.xml', 'parameters.json', 'log.txt', f'{module_name}_API.py']:
+        if filename in ['result.xml', 'parameters.json', 'log.txt', 'arima_API.py']:
             continue
         tar.add(filename)
